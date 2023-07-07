c2633eb0518f582755b4c9dccf98038e86db4afb
dycimage <2424748461@qq.com>
2023-07-07 08:01:01 +0800

    减少ui界面的压力

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def update_image():
     page = doc.load_page(current_page)
 
     # 将页面转换为图像
-    pix = page.get_pixmap(dpi=200,alpha=False)
+    pix = page.get_pixmap()
     img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
 
     # 对图像进行处理
